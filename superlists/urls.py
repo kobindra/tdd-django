d24ccce15998a10767d0aeed2be0903a66d67065
Kobie Crawford <kobie@crawfordnet.com>
2013-12-31 14:44:34 -0800

    Refactor to use list-specific URLs

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/one-for-now/$', 'lists.views.view_list', name='view_list'),
+
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
